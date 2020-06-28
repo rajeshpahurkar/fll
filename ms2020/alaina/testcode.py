95ba929964ae8ede1d30f0213bb7a620c0d69c41
menlosparks <66791099+menlosparks@users.noreply.github.com>
2020-06-28 17:25:03 -0400

    Add files via upload

---
+++ ms2020/alaina/testcode.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env pybricks-micropython
+# from pybricks.hubs import EV3Brick
+from pybricks import ev3brick as brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch
+from pybricks.robotics import DriveBase
+from pybricks.ev3devices import Motor
+ 
+#do not change above#
+
+
+SOUND_VOLUME=7
+ 
+ 
+def sound_happy():
+    brick.sound.beep(1100, 80, SOUND_VOLUME)
+    brick.sound.beep(900, 80, SOUND_VOLUME)
+sound_happy()
