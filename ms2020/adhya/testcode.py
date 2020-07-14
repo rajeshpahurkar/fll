de64d8a2fdb6b7b2cbebe6d5a4b26899dbbeaae6
menlosparks <66791099+menlosparks@users.noreply.github.com>
2020-07-14 18:42:05 -0400

    Add files via upload

--- ms2020/adhya/testcode.py
+++ ms2020/adhya/testcode.py
@@ -12,10 +12,15 @@ from pybricks.ev3devices import Motor
 
 
 WHEEL_DIAMETER_MM=89
+AXLE_TRACK_MM=157
+SENSOR_TO_AXLE=60
+
+WHEEL_DIAMETER_MM=89
 AXLE_TRACK_MM=135
+SENSOR_TO_AXLE=135
  
- #drive motors
-left_motor=Motor(Port.C, Direction.CLOCKWISE)
+#drive motors
+left_motor=Motor(Port.B, Direction.CLOCKWISE)
 right_motor=Motor(Port.D, Direction.CLOCKWISE)
 robot = DriveBase(left_motor, right_motor, WHEEL_DIAMETER_MM, AXLE_TRACK_MM)
 
@@ -45,8 +50,8 @@ def turn_arc(distance,angle):
 
 #turn_arc(distance=200,angle=90)
 
-#color_sensor_left = ColorSensor(Port.S2)
-color_sensor_right = ColorSensor(Port.S4)
+color_sensor_left = ColorSensor(Port.S4)
+color_sensor_right =None# ColorSensor(Port.S4)
 
 
 def move_to_color(
@@ -62,7 +67,7 @@ def move_to_color(
 
 #move_to_color(color_sensor=color_sensor_right, stop_on_color=Color.RED, speed_mm_s=100)
 
-touch_sensor= TouchSensor(Port.S3)
+touch_sensor=None# TouchSensor(Port.S3)
 
 def move_to_wall(touch_sensor, speed_mm_s):
    robot.drive(speed_mm_s, 0)
@@ -96,3 +101,73 @@ def calibrate_gyro(new_angle):
     wait(50)
 
 calibrate_gyro(new_angle=0)
+
+def turn_to_color(color_sensor, stop_on_color, angular_speed_deg_s):
+   robot.drive(0, angular_speed_deg_s)
+   # Check if color reached.
+   while color_sensor.color() != stop_on_color:
+       wait(10)
+   robot.stop(stop_type=Stop.BRAKE)
+
+#turn_to_color(color_sensor_left, Color.RED, 45)
+
+
+def turn_to_color_right(color_sensor, stop_on_color, angular_speed_deg_s):
+   robot.drive(0, angular_speed_deg_s)
+   # Check if color reached.
+   while color_sensor.color() != stop_on_color:
+       wait(10)
+   robot.stop(stop_type=Stop.BRAKE)
+
+#turn_to_color(color_sensor_right, Color.RED, 45)
+
+
+
+
+def move_crane_to_floor(crane_motor):
+   crane_motor.run_until_stalled(-180, Stop.COAST, 35)
+   move_crane_up( crane_motor, degrees = 5)
+
+#move_crane_to_floor(crane_motor)
+
+
+def log_string(message):
+   print(message)
+   brick.display.text(message)
+
+
+#log_string('Running robot now done')
+
+# Get wheel circumference
+WHEEL_CIRCUM_MM=3.149*89
+# 360 degrees -> WHEEL_CIRCUM_MM so   1 degree -> ?
+DEGREES_PER_MM=360/WHEEL_CIRCUM_MM
+
+
+def color_test(color_sensor, distance_mm, speed_mm_s):
+ 
+   left_motor.reset_angle(0)
+   motor_target_angle = int(DEGREES_PER_MM * distance_mm)
+ 
+   while (abs(left_motor.angle()) < abs(motor_target_angle)):
+       log_string('Color :' + str(color_sensor.color()) + ' Intense:' + str(color_sensor.reflection()))
+ 
+       robot.drive(speed_mm_s, 0)
+       wait(100)
+ 
+   robot.stop(stop_type=Stop.BRAKE)
+
+#color_test(color_sensor=color_sensor_left, distance_mm=150, speed_mm_s=50
+
+def move_straight(duration, speed_mm_s):
+     robot.drive_time(speed_mm_s, 0, duration)
+     robot.stop(stop_type=Stop.BRAKE)
+
+move_straight(duration=4000, speed_mm_s=300) 
+move_straight(5000, 300)
+
+
+
+
+
+
