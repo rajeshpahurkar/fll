ba26dce0ba5fe50030e99e76c299899946b91913
menlosparks <66791099+menlosparks@users.noreply.github.com>
2020-07-23 10:35:02 -0400

    Add files via upload

--- ms2020/samples/nxt_sensor.py
+++ ms2020/samples/nxt_sensor.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env pybricks-micropython
-import ev3dev.ev3 as ev3
+# import ev3dev.ev3 as ev3
 import pybricks.nxtdevices as nxt
 # from pybricks.nxtdevices import UltrasonicSensor
 import pybricks.ev3devices as ev3dev
@@ -9,13 +9,21 @@ from pybricks.ev3devices import UltrasonicSensor,TouchSensor
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 
 # us = ev3.UltrasonicSensor()
-# us = nxt.UltrasonicSensor(Port.S1)
+us = nxt.UltrasonicSensor(Port.S1)
+color_sensor_center = None##ColorSensor(Port.S4)
+touch_sensor= None## TouchSensor(Port.S2)
 message='Staring ultrasomic'
+brick.sound.beep(1100, 80, 7)
+print(message)
+brick.display.text(message)
 
 def test_sensor():
-    touch_sensor= TouchSensor(Port.S2)
-    while touch_sensor.pressed() == False :
-        wait(10)
-    brick.sound.beep(1100, 80, 7)
+    # while touch_sensor.pressed() == False :
+    while us.distance() > 10:
+        message='Distance ' + str(us.distance())
+        print(message)
+        brick.display.text(message)
+        wait(1000)
 
 # us = ev3dev.UltrasonicSensor(Port.S1)
+test_sensor()
