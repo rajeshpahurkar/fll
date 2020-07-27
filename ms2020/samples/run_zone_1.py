1e25d1977e13c60155dd393275a26e10e32c697d
menlosparks <66791099+menlosparks@users.noreply.github.com>
2020-07-27 08:04:06 -0400

    Add files via upload

---
+++ ms2020/samples/run_zone_1.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env pybricks-micropython
+# from pybricks.hubs import EV3Brick
+import sys
+import os
+from pybricks import ev3brick as brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch
+from pybricks.robotics import DriveBase
+from pybricks.ev3devices import Motor
+
+sys.path.append(os.path.abspath('../shared'))
+ 
+import robot_setup
+import testcode
+import mis_zone_1
+import bus_service_1
+
+from robot_setup import left_motor
+from robot_setup import right_motor
+from robot_setup import robot
+from robot_setup import rack_motor
+from robot_setup import crane_motor
+from robot_setup import gyro
+from robot_setup import touch_sensor 
+from robot_setup import color_sensor_left
+from robot_setup import color_sensor_right
+from robot_setup import color_sensor_center
+from robot_setup import touch_sensor
+from robot_setup import obstacle_sensor
+
+from robot_setup import SOUND_VOLUME
+from robot_setup import WHEEL_DIAMETER_MM
+from robot_setup import AXLE_TRACK_MM
+from robot_setup import SENSOR_TO_AXLE
+from robot_setup import WHEEL_CIRCUM_MM
+from robot_setup import DEGREES_PER_MM
+
+##### Do not change above this line ##########################################
+ 
+
+bus_service_1.bus_base_to_mis_1()
+
+mis_zone_1.mis_1_traffic()
+
+bus_service_1.bus_mis_1_to_mis_2()
+
+mis_zone_1.mis_2_hookedcrane()
+
+bus_service_1.bus_mis_2_to_home()
